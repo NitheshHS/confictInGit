19ec659246c195b8541f12fb6c2f05506d07fb4a
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-12-19 18:38:23 +0530

    second commit from different workSpace

--- src/practiceGit/PracticeGitConflict.java
+++ src/practiceGit/PracticeGitConflict.java
@@ -5,7 +5,8 @@ public class PracticeGitConflict {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first commit");
-
+		
+		System.out.println("first commit");
 	}
 
 }
