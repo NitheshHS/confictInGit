4a1a4de18a9a752c785865bcedf6e6d02267adca
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-12-19 18:51:16 +0530

    third commit from child

--- src/practiceGit/PracticeGitConflict.java
+++ src/practiceGit/PracticeGitConflict.java
@@ -6,7 +6,12 @@ public class PracticeGitConflict {
 		// TODO Auto-generated method stub
 		System.out.println("first commit");
 		
-		System.out.println("first commit");
+		System.out.println("second commit");
+		
+		System.out.println("third commit");
+
+		
+		
 	}
 
 }
